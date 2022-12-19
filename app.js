249ea300169ac112c4f181e3776aa09f23e6b40b
stopdaydreaming <hello@stopdaydreaming.com>
2022-12-19 11:30:01 -0500

    setup, added templates and data

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            title: 'the final empire',
+            author: 'brandon sanderson',
+            age: 45
+        }
+    }
+})
+
+app.mount('#app')
