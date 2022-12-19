1cb8a3333e11ca315a85553e3df15318ca80e5bc
stopdaydreaming <hello@stopdaydreaming.com>
2022-12-19 12:32:31 -0500

    added mouse events

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'the final empire',
             author: 'brandon sanderson',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleShowBooks(){
             this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data){
+            console.log(e, e.type)
+            if(data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
